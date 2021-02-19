588d925094ce5e9141396176266386a69728e08a
Dick Fennema <fennema.d@tkppensioen.nl>
2021-02-19 13:55:37 +0100

    aangepast

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          aanpassing in de feature
         </a>
       </header>
     </div>
